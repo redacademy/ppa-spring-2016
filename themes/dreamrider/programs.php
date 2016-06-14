6b1716ee1bbf6b3004f1ded6d73fcd95d9638640
kyleminchin1 <kyleminchin@hotmail.com>
2016-06-14 09:13:16 -0700

    html for About Us page

---
+++ themes/dreamrider/programs.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *  Template Name: Programs
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+	<div class="about-page-wrapper">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+				<div class="about-title">
+					<h1>about us</h1>
+				</div>
+				<section class="about-main">
+					<img src="" alt="">
+					<p>Through story, music, interactive media and live theatre, we create inclusive, meaningful, and full-throttle that children adore and that instill positive environmental values in their hearts - and in their actions. Kids feel energized, happy, and eager to put into practice what they have learned! Read more about our Mission and Values!</p>
+					<h5>"DreamRider's work to engage with young people throughout the region is critical to raising environmental literacy throughout our communities - we simply love that they do it in such a fun and creative way."</h5>
+					<h5>Christianne Wilhemson, Executive Director, Georgia Straight Alliance</h5>
+				</section><!-- about-main -->
+				<section class="about-story">
+					<p>We started as a project of the city of Vancouver in 1997, engaging elementary school kids in water conservation through theatre. Word spread, and pretty soon we were working with municipalities to reach schools, teachers, and kids throughout the Metro Vancouver Region. Due to popular demand we became our own independent non-profit Dream Rider Theatre Society in 2002, before becoming a registered charity in 2005.</p>
+					<p>Following the succes of our award-winning live theatre shows, we wanted to scale our reach and impact beyond Metro Vancouver to inspire a whole generation of environmental superheroes. The result is the Planet Protector Academy, a unique digital-led class-room based program in which kids become 'apprentice' Planet Protectors' and go home on superhero missions to change their families' behaviours!</p>
+				</section><!-- about-story -->
+				<section class="about-pictures">
+					
+				</section><!-- about-pictures -->
+				
+
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+				<?php endwhile; // End of the loop. ?>
+				
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- about-page-wrapper -->
+
+
+<?php get_footer(); ?>
