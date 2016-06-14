4ce3f133369e167f6b09c0d80d159788c9d81284
kyleminchin1 <kyleminchin@hotmail.com>
2016-06-14 11:18:09 -0700

    html structure complete site wide

--- themes/dreamrider/contact.php
+++ themes/dreamrider/contact.php
@@ -9,7 +9,11 @@ get_header(); ?>
 	<div class="contact-page-wrapper">
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
-
+				<section class="contact-header-section">
+					<h3>interested in knowing more?</h3>
+					<p>Get in touch with us at <span class="tel">604-939-0364</span></p>
+					<p>or send us a message below.</p>
+				</section>
 				<?php while ( have_posts() ) : the_post(); ?>
 
 					<?php get_template_part( 'template-parts/content', 'page' ); ?>
@@ -19,6 +23,6 @@ get_header(); ?>
 			</main><!-- #main -->
 		</div><!-- #primary -->
 	</div><!-- contact-page-wrapper -->
-	
+
 
 <?php get_footer(); ?>
