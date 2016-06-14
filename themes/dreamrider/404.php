8a799b791f30e101710a7de70694b472a89bc8bb
kyleminchin1 <kyleminchin@hotmail.com>
2016-06-14 13:42:45 -0700

    changes to 404 page

--- themes/dreamrider/404.php
+++ themes/dreamrider/404.php
@@ -14,41 +14,24 @@ get_header(); ?>
 
 				<section class="error-404 not-found">
 					<header class="page-header">
-						<h1 class="page-title"><?php echo esc_html( 'Oops! The page you are looking forcan&rsquo;t be found.' ); ?></h1>
+						<h1 class="big-text">Uh Oh</h1>
+						<h4 class="page-title">404 page not found</h4>
 					</header><!-- .page-header -->
 
 					<div class="page-content">
-						<p><?php echo esc_html( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?' ); ?></p>
-
-						<?php get_search_form(); ?>
-
-						<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-						<?php if ( red_starter_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
-						<div class="widget widget_categories">
-							<h2 class="widget-title"><?php echo esc_html( 'Most Used Categories' ); ?></h2>
-							<ul>
-							<?php
-								wp_list_categories( array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								) );
-							?>
-							</ul>
-						</div><!-- .widget -->
-						<?php endif; ?>
-
-						<?php 
-							/*
-							$archive_content = '<p>' . sprintf( esc_html( 'Try looking in the monthly archives. %1$s' ), convert_smilies( ':)' ) ) . '</p>';
-							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-							*/
-						?>
-
+						<img src="" alt="">
+						<h4>goober is working on it</h4>
 					</div><!-- .page-content -->
+					<p>The page you were looking for can't be found. But we have solutions for you.</p>
+
+					<div class="404-btn-container">
+						<div class="404-btn-a">
+							<p>return home</p>
+						</div>
+						<div class="404-btn-b">
+							<p>contact us</p>
+						</div>
+					</div>
 				</section><!-- .error-404 -->
 
 			</main><!-- #main -->
