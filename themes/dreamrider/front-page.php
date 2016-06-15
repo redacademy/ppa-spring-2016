87ad9a49e23521db583fb3d3a0145907bc262319
Claudine <claudine.bananal@gmail.com>
2016-06-14 21:31:46 -0700

    style logos area of community-partners section on frontpage

--- themes/dreamrider/front-page.php
+++ themes/dreamrider/front-page.php
@@ -127,27 +127,27 @@ get_header(); ?>
 				<a href="#">Read Case Studies</a>
 			</div>
 		</section><!-- community-impact -->
-
-		<section class="community-partners">
+		<hr>
+		<section class="community-partners-container">
 			<h2>Community Partners</h2>
 			<div class="partners-images">
 				<div class="partner-image">
-					<a href="http://www.brandon.ca"><img src="<?php echo get_template_directory_uri() ?>/images/community-partners/brandon.png" alt="City of Brandon Logo"></a>
+					<a href="http://www.brandon.ca" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/images/community-partners/brandon.png" alt="City of Brandon Logo"></a>
 				</div>
 				<div class="partner-image">
-					<a href="http://www.guelph.ca"><img src="<?php echo get_template_directory_uri() ?>/images/community-partners/guelph.png" alt="City of Guelph Logo"></a>
+					<a href="http://www.edmonton.ca" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/images/community-partners/edmonton.png" alt="City of Edmonton Logo"></a>
 				</div>
 				<div class="partner-image">
-					<a href="http://www.edmonton.ca"><img src="<?php echo get_template_directory_uri() ?>/images/community-partners/edmonton.png" alt="City of Edmonton Logo"></a>
+					<a href="http://www.city.peterborough.on.ca" target="_blank"><img src="<?php echo get_template_directory_uri()?>/images/community-partners/peterborough.png" alt="City of Peterborough Logo"></a>
 				</div>
 				<div class="partner-image">
-					<a href="http://www.vernon.ca"><img src="<?php echo get_template_directory_uri() ?>/images/community-partners/vernon.png" alt=""></a>
+					<a href="http://www.guelph.ca" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/images/community-partners/guelph.png" alt="City of Guelph Logo"></a>
 				</div>
 				<div class="partner-image">
-					<a href="http://www.city.peterborough.on.ca"><img src="<?php echo get_template_directory_uri()?>/images/community-partners/peterborough.png" alt="City of Peterborough Logo"></a>
+					<a href="http://www.vernon.ca" target="_blank"><img src="<?php echo get_template_directory_uri() ?>/images/community-partners/vernon.png" alt=""></a>
 				</div>
 				<div class="partner-image">
-					<a href="http://www.westvancouver.ca"><img src="<?php echo get_template_directory_uri()?>/images/community-partners/west-vancouver.png" alt="West Vancouver School District Logo"></a>
+					<a href="http://www.westvancouver.ca" target="_blank"><img src="<?php echo get_template_directory_uri()?>/images/community-partners/west-vancouver.png" alt="West Vancouver School District Logo"></a>
 				</div>
 			</div>
 			<!--end partners-images-->
