16086120eadd529c91bf88ba11580a551f77a4bf
Claudine <claudine.bananal@gmail.com>
2016-06-15 15:37:17 -0700

    refactor styling for dynamic footer

--- themes/dreamrider/footer.php
+++ themes/dreamrider/footer.php
@@ -6,22 +6,22 @@
  */
 
 ?>
- 
+
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="footer-container">
 					<div class="footer-nav">
-						
+
 						<!-- Back to Top Button -->
-						<p class="back-to-top">Back to top</p>
+						<p class="back-to-top">Back To Top</p>
 
 						<!-- Footer Menu -->
 						<?php echo wp_nav_menu( array(
 						    'menu' => 'Footer Menu',
 							'menu_class' => 'footer-menu'
 						)); ?>
-						
+
 					</div><!-- footer-nav -->
 					<div class="site-info">
 						<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
