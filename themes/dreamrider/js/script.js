4dd4aa4b3625d7ea5ce680f307fdfd132ea53a40
Billy <Billy>
2016-06-14 21:32:52 -0700

    Add back to top functionality

--- themes/dreamrider/js/script.js
+++ themes/dreamrider/js/script.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function( $ ) {
 
-	console.log("works!");
-
+	$('.back-to-top').click(function() {      // When arrow is clicked
+	    $('body,html').animate({ scrollTop : 0}, 500); // Scroll to top of body
+	});
+	
 });
