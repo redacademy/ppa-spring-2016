cf99144d1faeeecdcc2ffdd41ed39a5a484e0f85
Claudine <claudine.bananal@gmail.com>
2016-06-16 23:48:49 -0700

    draft styling of typography on about page

--- themes/dreamrider/about.php
+++ themes/dreamrider/about.php
@@ -16,14 +16,22 @@ get_header(); ?>
 					<div class="logo-container">
 						<img src="<?php echo get_template_directory_uri()?>/images/logos/logo-text-with-human.png" alt="DreamRider Logo">
 					</div>
-					<p>
+					<p class="about-intro">
 						Through story, music, interactive media and live theatre, we create inclusive, meaningful, and full-throttle that children adore and that instill positive environmental values in their hearts - and in their actions. Kids feel energized, happy, and eager to put into practice what they have learned! Read more about our Mission and Values!
 					</p>
-					<p class="about-quote">"DreamRider's work to engage with young people throughout the region is critical to raising environmental literacy throughout our communities - we simply love that they do it in such a fun and creative way."</h5>
-					<h5>Christianne Wilhemson, Executive Director, Georgia Straight Alliance</h5>
+					<div class="about-quote">
+						<p>
+							"DreamRider's work to engage with young people throughout the region is critical to raising environmental literacy throughout our communities - we simply love that they do it in such a fun and creative way."
+						</p>
+						<p class="quote-source">
+							Christianne Wilhemson, <br>Executive Director, <br>Georgia Straight Alliance
+						</p>
+					</div>
 				</section><!-- about-main -->
 				<section class="about-story">
-					<p>We started as a project of the city of Vancouver in 1997, engaging elementary school kids in water conservation through theatre. Word spread, and pretty soon we were working with municipalities to reach schools, teachers, and kids throughout the Metro Vancouver Region. Due to popular demand we became our own independent non-profit Dream Rider Theatre Society in 2002, before becoming a registered charity in 2005.</p>
+					<h3>The Dreamrider Story</h3>
+					<img src="<?php echo get_template_directory_uri()?>/images/YVR.png" alt="YVR Icon">
+					<p>We started as a project of the City of Vancouver in 1997, engaging elementary school kids in water conservation through theatre. Word spread, and pretty soon we were working with municipalities to reach schools, teachers, and kids throughout the Metro Vancouver Region. Due to popular demand we became our own independent non-profit Dream Rider Theatre Society in 2002, before becoming a registered charity in 2005.</p>
 					<p>Following the succes of our award-winning live theatre shows, we wanted to scale our reach and impact beyond Metro Vancouver to inspire a whole generation of environmental superheroes. The result is the Planet Protector Academy, a unique digital-led class-room based program in which kids become 'apprentice' Planet Protectors' and go home on superhero missions to change their families' behaviours!</p>
 				</section><!-- about-story -->
 				<section class="about-pictures">
