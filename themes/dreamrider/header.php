ff6ccda1d5f68296c2196df987fbaee9b93adf9a
Billy <Billy>
2016-06-20 14:11:20 -0700

    slider jquery fix

--- themes/dreamrider/header.php
+++ themes/dreamrider/header.php
@@ -21,14 +21,8 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-				<div class="dreamrider-header">
-					<img src="<?php echo get_template_directory_uri() ?>/images/logo.png" alt="Planet Protector Acadmey Logo">
-					<!-- <img src="<?php echo get_template_directory_uri() ?>/images/hamburger.png"> -->
-					
-				</div>
 				<div class="site-branding">
-					<!-- <h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1> -->
-					<!-- <p class="site-description"><?php bloginfo( 'description' ); ?></p> -->
+					<img src="<?php echo get_template_directory_uri() ?>/images/logo.png" alt="Planet Protector Acadmey Logo">
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
--- themes/dreamrider/js/script.js
+++ themes/dreamrider/js/script.js
@@ -1,22 +1,21 @@
-jQuery(document).ready(function() {
+(function($){
 
 	//Functionality: Back to Top Scroll
-	jQuery('.back-to-top').click(function() {      // On Click of Class
-	    jQuery('body,html').animate({ scrollTop : 0}, 1000); // Scroll to top of body
+	$('.back-to-top').click(function() {      // On Click of Class
+	    $('body,html').animate({ scrollTop : 0}, 1000); // Scroll to top of body
 	});
 
 	//Functionality: Back Button
-	jQuery('.back-btn').click(function(){	// On Click of Class
+	$('.back-btn').click(function(){	// On Click of Class
 		window.history.back(); //links back to the previous page of the browser
 	});
 
 	//initialize bxslider for Program Page
-	jQuery('.bxslider').bxSlider({
+	$('.bxslider').bxSlider({
 		pager:false
 	});
 
 	//Adds Modals class to the menu for Contact Pop up
-	jQuery("#menu-footer-menu li:last-child a").addClass("eModal-1");
+	$("#menu-footer-menu li:last-child a").addClass("eModal-1");
 
-
-});
+})(jQuery)
