c22aa6759c12e7bd6a862bedda88cb13410f758f
Billy <Billy>
2016-06-21 15:51:34 -0700

    Correct Header Style

--- themes/dreamrider/header.php
+++ themes/dreamrider/header.php
@@ -26,21 +26,23 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a> 
 
 			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
-					<img class="desktop-logo" src="<?php echo get_template_directory_uri() ?>/images/Logo-for-print-use.png" alt="Planet Protector Acadmey Logo">
-					<img class="mobile-logo" src="<?php echo get_template_directory_uri() ?>/images/Logo.png" alt="Planet Protector Acadmey Logo">
-				</div><!-- .site-branding -->
-				
-				<div class="desktop-nav">
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</div><!-- desktop-nav -->
-							
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?>
-						<i class="fa fa-bars dark-bar fa-2x" aria-hidden="true"></i>
-					</button>
+				<div class="header-container container">
+					<div class="site-branding">
+						<img class="desktop-logo" src="<?php echo get_template_directory_uri() ?>/images/Logo-for-print-use.png" alt="Planet Protector Acadmey Logo">
+						<img class="mobile-logo" src="<?php echo get_template_directory_uri() ?>/images/Logo.png" alt="Planet Protector Acadmey Logo">
+					</div><!-- .site-branding -->
 					
-				</nav><!-- #site-navigation -->
+					<div class="desktop-nav">
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+					</div><!-- desktop-nav -->
+								
+					<nav id="site-navigation" class="main-navigation" role="navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?>
+							<i class="fa fa-bars dark-bar fa-2x" aria-hidden="true"></i>
+						</button>
+						
+					</nav><!-- #site-navigation -->
+				</div>
 			</header><!-- #masthead -->
 			
 			<!-- Mobile Menu location -->
