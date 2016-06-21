499ead2d180c53257a836eea303af27b21bb23f3
Billy <Billy>
2016-06-20 23:18:41 -0700

    Add modify excerp to extra.php. Add link to image for blog archive

---
+++ themes/dreamrider/template-parts/content-blog.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		
+		<a href="<?php echo get_permalink() ?>""><img src ="<?php echo CFS()->get('post_image') ?>"></a>
+
+		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_excerpt(); ?>
+	</div><!-- .entry-content -->
+</article><!-- #post-## -->
