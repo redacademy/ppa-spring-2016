a797a3d980380ca614183610128af02ef4873c26
Claudine <claudine.bananal@gmail.com>
2016-06-22 11:49:56 -0700

    style thank-youpage

--- themes/dreamrider/template-parts/content-thank.php
+++ themes/dreamrider/template-parts/content-thank.php
@@ -12,7 +12,7 @@
 	<div class="entry-content">
 
 		<p><img src="<?php echo get_template_directory_uri() ?>/images/logo.png" alt=""></p>
-		<p><img src="<?php echo get_template_directory_uri() ?>/images/checkmark-brandcolor.png" alt=""></p>
+		<p><img class="checkmark-green" src="<?php echo get_template_directory_uri() ?>/images/checkmark-brandcolor.png" alt=""></p>
 		<?php the_content(); ?>
 		<div class="done">
 			<a href="/">Done</a>
