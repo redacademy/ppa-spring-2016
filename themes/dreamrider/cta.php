fdf77d1e53e0d48b85ab0f006e1d6f62127c80cd
kyleminchin1 <kyleminchin@hotmail.com>
2016-06-22 14:53:01 -0700

    creation of cta page

---
+++ themes/dreamrider/cta.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *  			Template Name: cta
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+	<div class="about-page-wrapper">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+	
+			</main><!-- #main -->
+		</div><!-- #primary -->
+	</div><!-- about-page-wrapper -->
+
+
+<?php get_footer(); ?>
