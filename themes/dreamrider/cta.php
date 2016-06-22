1c006d7429cf6a3d1229dab4129e3bc030c6d219
kyleminchin1 <kyleminchin@hotmail.com>
2016-06-22 16:56:47 -0700

    styling cta page buttons

--- themes/dreamrider/cta.php
+++ themes/dreamrider/cta.php
@@ -6,13 +6,29 @@
  */
 
 get_header(); ?>
-	<div class="about-page-wrapper">
+	
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
-	
+			<div class="cta-container">
+			<a href="">
+			<div class="cta-link">
+				<img src="<?php echo get_template_directory_uri() ?>/images/monitor-icon.png" alt="Monitor Icon">
+				<h3>Start my free trial</h3>
+				<p>get a feel for the product for my classroom</p>
+			</div>
+			</a>
+			<a href="">
+			<div class="cta-link">
+				<img src="<?php echo get_template_directory_uri() ?>/images/person-icon.png" alt="Person Icon">
+				<h3>Request a demo</h3>
+				<p>Get the full experience for my district or municipality</p>
+			</div>
+			</a>
+			</div><!-- cta-container -->
+
+
 			</main><!-- #main -->
 		</div><!-- #primary -->
-	</div><!-- about-page-wrapper -->
-
+	
 
 <?php get_footer(); ?>
