3bbe704898e6dcac999bd426ec02d58700317ec0
Billy <Billy>
2016-06-22 10:36:46 -0700

    added thank you page

---
+++ themes/dreamrider/template-parts/content-thank.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template part for displaying posts.
+ *
+ * @package RED_Starter_Theme
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<div class="entry-content">
+
+		<p><img src="<?php echo get_template_directory_uri() ?>/images/logo.png" alt=""></p>
+		<p><img src="<?php echo get_template_directory_uri() ?>/images/checkmark-brandcolor.png" alt=""></p>
+		<?php the_content(); ?>
+		<div class="done">
+			<a href="/">Done</a>
+		</div>
+
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## -->
