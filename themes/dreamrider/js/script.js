03b975095bb695994418db5877fd202fd2d5847e
Billy <Billy>
2016-06-22 23:54:25 -0700

    Modified script.js

--- themes/dreamrider/js/script.js
+++ themes/dreamrider/js/script.js
@@ -16,12 +16,14 @@ jQuery(document).ready(function($) {
 
 
 	//Functionality: Scroll to Learning Benefits Section
-	$('#menu-item-118 a').click(function(){  //On Click of Class
+	$('#menu-item-118 a').click(function(e){  //On Click of Class
+		e.preventDefault();
 		$('body,html').animate({ scrollTop : $('.learning-benefits').offset().top - 100}, 1000); //Scroll to section with class name
 	});
 
 	//Functionality: Scroll to Community Section
-	$('#menu-item-119 a').click(function(){  //On Click of Class
+	$('#menu-item-119 a').click(function(e){  //On Click of Class
+		e.preventDefault();
 		$('body,html').animate({ scrollTop : $('.community-impact').offset().top - 100}, 1000); //Scroll to section with class name
 	});
 
