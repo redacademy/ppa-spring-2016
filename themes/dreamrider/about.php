3bf171b2f0d2011e7ac9673bbd07eb0f8980a13b
Claudine <claudine.bananal@gmail.com>
2016-06-23 16:28:08 -0700

    change about-intro to div

--- themes/dreamrider/about.php
+++ themes/dreamrider/about.php
@@ -17,11 +17,11 @@ get_header(); ?>
 						<img class="text-logo" src="<?php echo get_template_directory_uri()?>/images/logos/dreamrider-logo-text.jpg" alt="DreamRider Logo">
 						<img class="logo-with-human" src="<?php echo get_template_directory_uri()?>/images/logos/dreamrider-logo-with-characters.jpg" alt="DreamRider Logo">
 					</div>
-					<p class="about-intro container">
+					<div class="about-intro container">
 						<?php while ( have_posts() ) : the_post(); ?>
 							<?php the_content(); ?>
 						<?php endwhile; // End of the loop. ?>
-					</p>
+					</div>
 					<div class="about-quote">
 						<div class="container">
 							<p>
