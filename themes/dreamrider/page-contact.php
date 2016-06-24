2a3d4886dc51911ca59d6fd508f561a20d22f0dc
Claudine <claudine.bananal@gmail.com>
2016-06-24 13:32:23 -0700

    change I to capital on contact page

--- themes/dreamrider/page-contact.php
+++ themes/dreamrider/page-contact.php
@@ -10,7 +10,7 @@ get_header(); ?>
 			<main id="main" class="site-main contact-main" role="main">
 				<section class="contact-header-section">
 					<img class="mobile-logo" src="<?php echo get_template_directory_uri() ?>/images/logos/ppa-logo-stand-alone.png" alt="Planet Protector Acadmey Logo">
-					<h3>interested in knowing more?</h3>
+					<h3>Interested in knowing more?</h3>
 					<p class="middle">Get in touch with us at <span class="tel">604-939-0364</span></p>
 					<p>or send us a message below.</p>
 				</section>
