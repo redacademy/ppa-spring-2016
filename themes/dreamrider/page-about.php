6db02d5d6f54fa84b05d5f50c8a8902d2032b958
Claudine <claudine.bananal@gmail.com>
2016-06-28 13:50:08 -0700

    change typography of testiomonial source on about page

--- themes/dreamrider/page-about.php
+++ themes/dreamrider/page-about.php
@@ -10,8 +10,8 @@ get_header(); ?>
 			<main id="main" class="site-main" role="main">
 				<div class="about-title">
 					<h1>About Us</h1>
-				</div>			
-				
+				</div>
+
 				<section class="about-main">
 					<div class="logo-container container">
 						<img class="text-logo" src="<?php echo get_template_directory_uri()?>/images/logos/dreamrider-logo-text.jpg" alt="DreamRider Logo">
@@ -27,24 +27,24 @@ get_header(); ?>
 							<p>
 								<?php echo esc_html(CFS()->get('about_quote')) ?>
 							</p>
-							<p class="quote-source">
+							<p>
 								<?php echo esc_html(CFS()->get('about_quote_source')) ?>
 							</p>
 						</div>
 					</div>
 				</section><!-- about-main -->
-	
+
 				<section class="about-story">
 					<h3>The Dreamrider Story</h3>
 					<img src="<?php echo get_template_directory_uri()?>/images/YVR.png" alt="YVR Icon">
 					<p><?php echo esc_html(CFS()->get('about_story')) ?></p>
 				</section><!-- about-story -->
-	
+
 				<section class="about-pictures">
 					<div><img src="<?php echo CFS()->get('about_image_1') ?>"></div>
 					<div><img src="<?php echo CFS()->get('about_image_2') ?>"></div>
 				</section><!-- about-pictures -->
-	
+
 				<section class="about-today">
 					<div class="container">
 						<h3>Today</h3>
@@ -54,7 +54,7 @@ get_header(); ?>
 
 				<section class="about-community container">
 					<h3>2016 Community Partners</h3>
-					
+
 					<?php get_template_part('template-parts/community-partners') ?>
 					<p>
 						Help us to continue spreading the DreamRider magic by donating online through CanadaHelps.
